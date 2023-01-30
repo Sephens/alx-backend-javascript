66ff395b0367d8e226a5dc92d335dc31f7da78a8
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2023-01-30 22:40:26 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString && typeof startString === 'String') {
+  if (startString && typeof startString === 'string') {
     const strings = [];
     for (const item of set) {
       if (item.startsWith(startString)) {
